2d889363893695289296d4d5eca2a2ad3429614a
gavincook <swbyzx@gmail.com>
2015-03-27 23:47:48 +0800

    .添加支付完成跳转

--- src/main/webapp/js/pages/cddtsc/pay.js
+++ src/main/webapp/js/pages/cddtsc/pay.js
@@ -3,21 +3,25 @@
     $(function(){
         //支付按钮事件
         $(".pay-btn").click(function(){
-           if($("[name='password']").val().length<6){//密码小于6位
-               moon.error("请输入正确的支付密码");
-               return false;
-           }
-           $("#payForm").ajaxSubmitForm(contextPath+"/order/pay").done(function(data){
-               if(data.success){
-
-               }else{
-                   if(data.result) {
-                       moon.error(data.result.errMsg);
-                   }else{
-                       moon.error(data.throwable.localizedMessage);
-                   }
-               }
-           })
+            console.log(",,,,");
+             if($("[name='password']").val().length<6){//密码小于6位
+                 moon.error("请输入正确的支付密码");
+                return false;
+            }
+            var $btn = $(".pay-btn");
+            $btn.addClass("disabled").html("付款确认中...");
+            $("#payForm").ajaxSubmitForm(contextPath+"/order/pay").done(function(data){
+                if(data.success){
+                    window.location.href = contextPath+"/order/list_bought_items.html";
+                }else{
+                    if(data.result) {
+                        moon.error(data.result.errMsg);
+                    }else{
+                        moon.error(data.throwable.localizedMessage);
+                    }
+                    $btn.removeClass("disabled").html("付款");
+                }
+            });
        });
     });
 })();
